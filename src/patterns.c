0aaad96ae17c94a9d24566f96e6cad5cf21518be
aanciaes <aanciaes.miguel@gmail.com>
2018-11-28 21:58:24 +0000

    Added scan tree implementation as well as array implementation

--- src/patterns.c
+++ src/patterns.c
@@ -53,6 +53,48 @@ void reduce (void *dest, void *src, size_t nJob, size_t sizeJob, void (*worker)(
      }
 }
 
+void upsweepTree (void* src, size_t lo, size_t hi, struct ScanNode* node, size_t sizeJob, void (*worker)(void *v1, const void *v2, const void *v3)) {
+    if (lo + 1 == hi){
+        node->sum = *(TYPE *)(src + lo * sizeJob);
+        node->index = lo;
+    } else {
+        size_t mid = (lo + hi) / 2;
+
+        node->left = malloc(sizeof(struct ScanNode));
+        node->right = malloc(sizeof(struct ScanNode));
+        node->index = -1;
+        cilk_spawn upsweepTree (src, lo, mid, node->left, sizeJob, worker);     
+        upsweepTree (src, mid, hi, node->right, sizeJob, worker);
+        
+        cilk_sync;
+        worker(&(node->sum), &(node->left->sum), &(node->right->sum));
+    }
+}
+
+void downsweepTree (void* src, void* dest, size_t sizeJob, struct ScanNode* node, TYPE fromLeft, void (*worker)(void *v1, const void *v2, const void *v3)) {
+    
+    if(node->index != -1){
+            worker(dest + node->index * sizeJob, src + node->index * sizeJob, &fromLeft);
+    }else{
+    
+        cilk_spawn downsweepTree(src, dest, sizeJob, node->left, fromLeft, worker);
+        downsweepTree(src, dest, sizeJob, node->right, fromLeft + node->left->sum, worker);
+        
+        cilk_sync;
+    }
+}
+
+void scanTree (void *dest, void *src, size_t nJob, size_t sizeJob, void (*worker)(void *v1, const void *v2, const void *v3)) {
+    /* To be implemented */
+    assert (dest != NULL);
+    assert (src != NULL);
+    assert (worker != NULL);
+
+    struct ScanNode* parent = malloc(sizeof(struct ScanNode));
+    upsweepTree (src, 0, nJob, parent, sizeJob, worker);
+    downsweepTree (src, dest, sizeJob, parent, 0, worker);
+}
+
  
 void upsweep( void* src, void* aux, size_t lo, size_t hi,  size_t sizeJob, void (*worker)(void *v1, const void *v2, const void *v3)) {
     if( lo + 1 == hi ) {
@@ -66,10 +108,7 @@ void upsweep( void* src, void* aux, size_t lo, size_t hi,  size_t sizeJob, void 
         
 		cilk_sync;
         
-        //printf("lo: %zd, hi: %zd, sum: %p + %p\n",lo, hi, initialPos + (mid*sizeJob - sizeJob), initialPos + (hi*sizeJob - sizeJob));		
 		worker(aux + (hi*sizeJob - sizeJob), aux + (mid*sizeJob - sizeJob), aux + (hi*sizeJob - sizeJob));
-
-        //printf("Result: %lf\n", *(TYPE *)(aux + (hi*sizeJob - sizeJob)));
     }
 }
 
